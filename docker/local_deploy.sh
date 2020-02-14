42c37d5a11493ab5ea61ac0a47e5812f99b6eafe
Alex McNerney <ajm13@me.com>
2020-02-14 11:11:06 -0800

    Update docker stuffs

--- docker/local_deploy.sh
+++ docker/local_deploy.sh
@@ -1,18 +1,12 @@
 if [ "$(docker ps -aq -f name=chemical-properties)" != "" ]; then
-        # cleanup
-        echo "removing exited container"
-        docker rm -f chemical-properties
+    # cleanup
+    echo "removing exited container"
+    docker rm -f chemical-properties
 fi
 
-if [ "$1" != "" ]; then
-    echo "Using specified plugin server: $1"
-    docker run -d \
-    -p 8888:8888 \
-    -e PLUGIN_SERVER=$1 \
-    --name chemical-properties chemical-properties
-else
-    echo "Using default plugin server: plugins.nanome.ai"
-    docker run -d \
-    -p 8888:8888 \
-    --name chemical-properties chemical-properties
-fi
+ARGS=$*
+
+docker run -d \
+--restart always \
+-e ARGS="$ARGS" \
+--name chemical-properties chemical-properties
